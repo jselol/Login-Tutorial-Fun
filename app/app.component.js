6763c32adb6e04fc1563b3a867f9b87908d6124f
jselol <jesse.williams2690@gmail.com>
2017-01-04 12:02:04 -0500

    pushing new updates for loading in data using canned json data file, also updates to app structure - more structural changes to come.

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './signup.component', './login.component', './userschedule.component', 'angular2/router'], function(exports_1, context_1) {
+System.register(['angular2/core', './signup.component', './login.component', './userschedule.component', 'angular2/router', './authentication.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './signup.component', './login.component', './
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, signup_component_1, login_component_1, userschedule_component_1, router_1;
+    var core_1, signup_component_1, login_component_1, userschedule_component_1, router_1, authentication_service_1;
     var AppComponent;
     return {
         setters:[
@@ -28,24 +28,31 @@ System.register(['angular2/core', './signup.component', './login.component', './
             },
             function (router_1_1) {
                 router_1 = router_1_1;
+            },
+            function (authentication_service_1_1) {
+                authentication_service_1 = authentication_service_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent() {
+                function AppComponent(_authenticationService, _userSchedule) {
+                    this._authenticationService = _authenticationService;
+                    this._userSchedule = _userSchedule;
+                    this._authenticationService.getAllUsers();
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
                         directives: [signup_component_1.SignupComponent, login_component_1.LoginComponent, router_1.ROUTER_DIRECTIVES],
+                        providers: [authentication_service_1.AuthenticationService, userschedule_component_1.UserscheduleComponent],
                         template: "\n            <router-outlet></router-outlet>\n        "
                     }),
                     router_1.RouteConfig([
-                        { path: '/home', name: 'Home', component: signup_component_1.SignupComponent, useAsDefault: true },
+                        { path: '/home', name: 'Home', component: login_component_1.LoginComponent, useAsDefault: true },
                         { path: '/signup', name: 'Signup', component: signup_component_1.SignupComponent },
                         { path: '/login', name: 'Login', component: login_component_1.LoginComponent },
                         { path: '/userschedule', name: 'Userschedule', component: userschedule_component_1.UserscheduleComponent }
                     ]), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [authentication_service_1.AuthenticationService, userschedule_component_1.UserscheduleComponent])
                 ], AppComponent);
                 return AppComponent;
             }());
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,36 @@
-import {Component} from 'angular2/core';
+import {Component, Injectable} from 'angular2/core';
 import {SignupComponent} from './signup.component';
 import {LoginComponent} from './login.component';
 import {PrivateComponent} from './private.component';
 import {UserscheduleComponent} from './userschedule.component';
+// import {adminscheduleComponent} from './adminschedule.component';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Schedule} from './schedule';
+import {AuthenticationService} from './authentication.service';
+import {User} from './user'
+
 
 @Component({
     selector: 'my-app',
     directives: [SignupComponent, LoginComponent, ROUTER_DIRECTIVES],
+    providers: [AuthenticationService, UserscheduleComponent],
     template: `
             <router-outlet></router-outlet>
         `
 })
 @RouteConfig([
-    { path: '/home', name: 'Home', component: SignupComponent, useAsDefault:true },
+    { path: '/home', name: 'Home', component: LoginComponent, useAsDefault:true },
     { path: '/signup', name: 'Signup', component: SignupComponent },
     { path: '/login', name: 'Login', component: LoginComponent },
     { path: '/userschedule', name: 'Userschedule', component: UserscheduleComponent}
 ])
 export class AppComponent {
+  schedule: Array<Schedule>;
+
+  constructor(private _authenticationService: AuthenticationService, private _userSchedule: UserscheduleComponent)
+  {
+   this._authenticationService.getAllUsers();
+  }
+
 
 }
